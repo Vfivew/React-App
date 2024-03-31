6dc2cb774ffbaa3dcb74e51e75e0cd618d5d692f
Vfivew <hitori555111555@yahoo.com>
2024-03-31 14:34:43 +0300

    feat(backend): delete base config data tm-4

--- backend/src/libs/config/typeorm.ts
+++ backend/src/libs/config/typeorm.ts
@@ -1,26 +1,26 @@
 import { registerAs } from '@nestjs/config';
 import { config as dotenvConfig } from 'dotenv';
-import { DataSource, DataSourceOptions } from 'typeorm';
+import { DataSource, DataSourceOptions } from "typeorm";
 import { Task } from '../../modules/tasks/entities/task.entity';
 import { TaskColumn } from '../../modules/task-columns/entities/task-column.entity';
-import { HistoryOfChangesTaskModule } from 'src/modules/history-of-changes-task/history-of-changes-task.module';
+import { HistoryOfChangesTaskModule } from '../../modules/history-of-changes-task/history-of-changes-task.module';
 
 dotenvConfig({ path: '.env' });
 
 const config = {
-  type: 'postgres',
-  host: 'postgres://board_vji9_user:jVHK4rWlBauFbKSuUbLEwZMelS3fjZsS@dpg-co4hq5sf7o1s738sepe0-a.frankfurt-postgres.render.com/board_vji9',
-  port: '5432',
-  username: 'board_vji9_user',
-  password: 'jVHK4rWlBauFbKSuUbLEwZMelS3fjZsS',
-  database: 'board_vji9',
-  entities: [Task, TaskColumn, HistoryOfChangesTaskModule],
-  migrations: ['dist/libs/db/migrations/*{.ts,.js}'],
-  autoLoadEntities: true,
-  synchronize: false,
-  migrationsRun: true,
-  migrationsTableName: 'migrations',
-};
+    type: 'postgres',
+    host: `${process.env.DATABASE_HOST}`,
+    port: `${process.env.DATABASE_PORT}`,
+    username: `${process.env.DATABASE_USERNAME}`,
+    password: `${process.env.DATABASE_PASSWORD}`,
+    database: `${process.env.DATABASE_NAME}`,
+    entities: [Task, TaskColumn, HistoryOfChangesTaskModule],
+    migrations: ["dist/libs/db/migrations/*{.ts,.js}"],
+    autoLoadEntities: true,
+    synchronize: false,
+    migrationsRun: true,
+    migrationsTableName: "migrations"
+}
 
-export default registerAs('typeorm', () => config);
+export default registerAs('typeorm', () => config)
 export const connectionSource = new DataSource(config as DataSourceOptions);
